c1243fc8776716b587f176c903a68339a25c5727
Christian Gray <educationmee@gmail.com>
2018-12-15 11:49:27 -0600

    added hello world

--- ApplePie123/ViewController.swift
+++ ApplePie123/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let messege = "hello world"
+        
+        print(messege)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
