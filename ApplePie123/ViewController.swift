40c68d965a5191e4a9b406e5fd0428741d91c9d5
Christian Gray <educationmee@gmail.com>
2018-12-15 11:55:51 -0600

    Added revered functionality to text

--- ApplePie123/ViewController.swift
+++ ApplePie123/ViewController.swift
@@ -16,9 +16,12 @@ class ViewController: UIViewController {
         let messege = "hello world"
         
         print(messege)
+        print(reverse(text: "stressed"))
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func reverse(text:String) -> String {
+        return String(text.reversed())
+    }
 
 }
 
